758784b6bc2637e35d0eee89e3884ca016ba552f
tritan89 <61888066+tritan89@users.noreply.github.com>
2023-09-11 19:57:50 -0700

    added functionalty

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import { nanoid } from "nanoid";
 
-function App() {
+
+
+
+function App(props:any) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map((task:any)=>  <Todo id={task.id} name={task.name} completed={task.completed} key ={task.id} toggleTaskCompleted={toggleTaskCompleted}/>)
+  function addTask(name:string){
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask])
+  }
+  function toggleTaskCompleted(id:any) {
+    console.log(tasks[0]);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   < div className="todoapp stack-large">
+    <h1>TodoMatic</h1>
+   <Form addTask={addTask}/>
+    <div className="filters btn-group stack-exception">
+      <FilterButton name = "all"/>
+      <FilterButton name = "finished"/>
+      <FilterButton name = "unfinished" />
     </div>
+    <h2 id="list-heading">{taskList.length} tasks remaining</h2>
+    <ul
+      role="list"
+      className="todo-list stack-large stack-exception"
+      aria-labelledby="list-heading">
+     
+      {taskList}
+    </ul>
+    
+  </div>
+
+
   );
 }
 
