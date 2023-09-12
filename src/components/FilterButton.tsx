d81fe1159fc66d290d858781dcaa7bee8a1a5f9f
tritan89 <61888066+tritan89@users.noreply.github.com>
2023-09-11 20:26:00 -0700

    update

---
+++ src/components/FilterButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FilterButton = (props:any) => {
+  return (
+    
+      <button type="button" className="btn toggle-btn" aria-pressed="true">
+        <span className="visually-hidden">Show </span>
+        <span>{props.name}</span>
+        <span className="visually-hidden"> tasks</span>
+      </button>
+    
+  )
+}
+
+export default FilterButton
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const DATA = [
+  { id: "td_0", name: "Eat", completed: true },
+  { id: "td_1", name: "gym", completed: false },
+  { id: "td_2", name: "study", completed: false },
+];
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
