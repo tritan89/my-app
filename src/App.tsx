f7694f2b65fdae8b01d538c5515a1ef7c33bfca3
tritan89 <61888066+tritan89@users.noreply.github.com>
2023-09-17 14:27:50 -0700

    deleting and adding tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,63 @@
 import React, { useState } from "react";
-import Todo from './components/Todo';
-import Form from './components/Form';
-import FilterButton from './components/FilterButton';
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 
-
-
-
-function App(props:any) {
+function App(props: any) {
   const [tasks, setTasks] = useState(props.tasks);
 
-  const taskList = tasks.map((task:any)=>  <Todo id={task.id} name={task.name} completed={task.completed} key ={task.id} toggleTaskCompleted={toggleTaskCompleted}/>)
-  function addTask(name:string){
+  const taskList = tasks.map((task: any) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  function addTask(name: string) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
-    setTasks([...tasks, newTask])
+    setTasks([...tasks, newTask]);
   }
-  function toggleTaskCompleted(id:any) {
-    console.log(tasks[0]);
+  function toggleTaskCompleted(id: any) {
+    const updatedTasks = tasks.map((task: any) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   }
-  
+
+  function deleteTask(id: any) {
+    const remainingTasks = tasks.filter((task: any) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
   return (
-   < div className="todoapp stack-large">
-    <h1>TodoMatic</h1>
-   <Form addTask={addTask}/>
-    <div className="filters btn-group stack-exception">
-      <FilterButton name = "all"/>
-      <FilterButton name = "finished"/>
-      <FilterButton name = "unfinished" />
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton name="all" />
+        <FilterButton name="finished" />
+        <FilterButton name="unfinished" />
+      </div>
+      <h2 id="list-heading">{taskList.length} tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
-    <h2 id="list-heading">{taskList.length} tasks remaining</h2>
-    <ul
-      role="list"
-      className="todo-list stack-large stack-exception"
-      aria-labelledby="list-heading">
-     
-      {taskList}
-    </ul>
-    
-  </div>
-
-
   );
 }
 
